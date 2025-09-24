1dcbc6df6d5cfa94fc36e2a093d4c2bb6ac60d64
Jonathanflesch <jonathanflesch10@gmail.com>
2025-09-24 17:18:21 -0300

    ex4

--- exe4/main.c
+++ exe4/main.c
@@ -20,10 +20,64 @@
   * 1..2.0V: 150 ms
   * 2..3.3V: 400 ms
   */
+ double leitura = 0.0;
+
+ volatile int timer_flag = 0;
+
+volatile int timer_run;
+ void adc_task(void *p) {
+    adc_init();
+    adc_gpio_init(28);
+
+    
+    while (1) {
+        uint16_t result;
+        adc_select_input(2); // Select ADC input 1 (GPIO27)
+        result = adc_read();
+        leitura = result*conversion_factor;
+        //printf("%f",leitura);
+
+    }
+}
+
+
+bool timer_callback(repeating_timer_t *rt){
+    timer_flag = 1;
+    return true;
+}
+
+
  int main() {
      stdio_init_all();
 
+     repeating_timer_t tim;
+
+     int led =0;
+
      while (1) {
-   
+
+        if(leitura <= 1){
+            if(timer_run){
+                cancel_repeating_timer(&tim);
+            }
+        }
+        if(leitura <= 2 && leitura > 1){
+            if(add_repeating_timer_ms(300,timer_callback,NULL,&tim)){
+                timer_run = 1;
+            }
+        }
+        if(leitura <= 3.3 && leitura > 2){
+            if(add_repeating_timer_ms(500,timer_callback,NULL,&tim)){
+                timer_run = 1;
+            }
+        }
+
+        if(timer_flag){
+            timer_flag = 0;
+            if(timer_run){
+                led = !led;
+                gpio_put(PIN_LED_B,led);
+            }
+        }
      }
  }
